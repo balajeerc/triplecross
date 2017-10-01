4ee833d9e9196f04c73d51f3b4bd00fac0ebb876
Balajee Ramachandran <balajeerc@gmail.com>
2017-10-01 12:36:52 +0530

    Minimal boilerplate - webpack, babel and react

---
+++ src/testModule.js
@@ -0,0 +1,4 @@
+const powerUp = (inputArray) => inputArray.map((k) => k**2);
+
+// export default saySomething;
+export default powerUp;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'cheap-eval-src-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    chunks: true,
+    reasons: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
